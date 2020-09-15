bd75d4310f73d491ae63cb8e4990ab01d7a41b49
Teerarak <teerarak@builk.com>
2020-09-15 14:23:10 +0700

    remove get

--- Cal/Controllers/CalController.cs
+++ Cal/Controllers/CalController.cs
@@ -23,18 +23,6 @@ namespace Cal.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
         [HttpPost("Pow")]
         public double power(double num1, double num2)
         {
